06fccec92b602f3499043c491330c0ef3b0dea94
Serhii Lozhechka <serhii.lozhechka@globallogic.com>
2017-05-17 17:33:53 +0300

    Init state

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace media_player_demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.videoPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.timer = new System.Windows.Forms.Timer();
+            this.openkey = new System.Windows.Forms.PictureBox();
+            this.playKey = new System.Windows.Forms.PictureBox();
+            this.pauseKey = new System.Windows.Forms.PictureBox();
+            this.stopKey = new System.Windows.Forms.PictureBox();
+            this.muteKey = new System.Windows.Forms.PictureBox();
+            this.lessVolumeKey = new System.Windows.Forms.PictureBox();
+            this.moreVolumeKey = new System.Windows.Forms.PictureBox();
+            this.removeKey = new System.Windows.Forms.PictureBox();
+            this.statusBar = new System.Windows.Forms.TextBox();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.videoPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // videoPanel
+            // 
+            this.videoPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.videoPanel.Controls.Add(this.pictureBox1);
+            this.videoPanel.Location = new System.Drawing.Point(18, 13);
+            this.videoPanel.Name = "videoPanel";
+            this.videoPanel.Size = new System.Drawing.Size(480, 320);
+            this.videoPanel.DoubleClick += new System.EventHandler(this.ToggleFullScreen);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(189, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 100);
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.ToggleFullScreen);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // openkey
+            // 
+            this.openkey.Image = ((System.Drawing.Image)(resources.GetObject("openkey.Image")));
+            this.openkey.Location = new System.Drawing.Point(18, 369);
+            this.openkey.Name = "openkey";
+            this.openkey.Size = new System.Drawing.Size(30, 23);
+            this.openkey.Click += new System.EventHandler(this.openkey_Click);
+            // 
+            // playKey
+            // 
+            this.playKey.Image = ((System.Drawing.Image)(resources.GetObject("playKey.Image")));
+            this.playKey.Location = new System.Drawing.Point(56, 369);
+            this.playKey.Name = "playKey";
+            this.playKey.Size = new System.Drawing.Size(30, 23);
+            this.playKey.Click += new System.EventHandler(this.playKey_Click);
+            // 
+            // pauseKey
+            // 
+            this.pauseKey.Image = ((System.Drawing.Image)(resources.GetObject("pauseKey.Image")));
+            this.pauseKey.Location = new System.Drawing.Point(94, 369);
+            this.pauseKey.Name = "pauseKey";
+            this.pauseKey.Size = new System.Drawing.Size(30, 23);
+            this.pauseKey.Click += new System.EventHandler(this.pauseKey_Click);
+            // 
+            // stopKey
+            // 
+            this.stopKey.Image = ((System.Drawing.Image)(resources.GetObject("stopKey.Image")));
+            this.stopKey.Location = new System.Drawing.Point(132, 369);
+            this.stopKey.Name = "stopKey";
+            this.stopKey.Size = new System.Drawing.Size(30, 23);
+            this.stopKey.Click += new System.EventHandler(this.stopKey_Click);
+            // 
+            // muteKey
+            // 
+            this.muteKey.Image = ((System.Drawing.Image)(resources.GetObject("muteKey.Image")));
+            this.muteKey.Location = new System.Drawing.Point(361, 367);
+            this.muteKey.Name = "muteKey";
+            this.muteKey.Size = new System.Drawing.Size(30, 23);
+            this.muteKey.Click += new System.EventHandler(this.muteKey_Click);
+            // 
+            // lessVolumeKey
+            // 
+            this.lessVolumeKey.Image = ((System.Drawing.Image)(resources.GetObject("lessVolumeKey.Image")));
+            this.lessVolumeKey.Location = new System.Drawing.Point(397, 367);
+            this.lessVolumeKey.Name = "lessVolumeKey";
+            this.lessVolumeKey.Size = new System.Drawing.Size(30, 23);
+            this.lessVolumeKey.Click += new System.EventHandler(this.lessVolumeKey_Click);
+            // 
+            // moreVolumeKey
+            // 
+            this.moreVolumeKey.Image = ((System.Drawing.Image)(resources.GetObject("moreVolumeKey.Image")));
+            this.moreVolumeKey.Location = new System.Drawing.Point(433, 367);
+            this.moreVolumeKey.Name = "moreVolumeKey";
+            this.moreVolumeKey.Size = new System.Drawing.Size(30, 23);
+            this.moreVolumeKey.Click += new System.EventHandler(this.moreVolumeKey_Click);
+            // 
+            // removeKey
+            // 
+            this.removeKey.Image = ((System.Drawing.Image)(resources.GetObject("removeKey.Image")));
+            this.removeKey.Location = new System.Drawing.Point(469, 367);
+            this.removeKey.Name = "removeKey";
+            this.removeKey.Size = new System.Drawing.Size(30, 23);
+            this.removeKey.Click += new System.EventHandler(this.removeKey_Click);
+            // 
+            // statusBar
+            // 
+            this.statusBar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.statusBar.Enabled = false;
+            this.statusBar.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular);
+            this.statusBar.ForeColor = System.Drawing.Color.Black;
+            this.statusBar.Location = new System.Drawing.Point(229, 340);
+            this.statusBar.Multiline = true;
+            this.statusBar.Name = "statusBar";
+            this.statusBar.ReadOnly = true;
+            this.statusBar.Size = new System.Drawing.Size(270, 21);
+            this.statusBar.TabIndex = 48;
+            this.statusBar.Text = "(Status)";
+            // 
+            // filePath
+            // 
+            this.filePath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.filePath.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular);
+            this.filePath.ForeColor = System.Drawing.Color.Black;
+            this.filePath.Location = new System.Drawing.Point(18, 340);
+            this.filePath.Name = "filePath";
+            this.filePath.ReadOnly = true;
+            this.filePath.Size = new System.Drawing.Size(210, 21);
+            this.filePath.TabIndex = 58;
+            this.filePath.Text = "(No file loaded)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(516, 400);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.removeKey);
+            this.Controls.Add(this.moreVolumeKey);
+            this.Controls.Add(this.lessVolumeKey);
+            this.Controls.Add(this.muteKey);
+            this.Controls.Add(this.stopKey);
+            this.Controls.Add(this.pauseKey);
+            this.Controls.Add(this.playKey);
+            this.Controls.Add(this.openkey);
+            this.Controls.Add(this.videoPanel);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Video Playback in VC#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Closed += new System.EventHandler(this.Form1_closing);
+            this.videoPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel videoPanel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox openkey;
+        private System.Windows.Forms.PictureBox playKey;
+        private System.Windows.Forms.PictureBox pauseKey;
+        private System.Windows.Forms.PictureBox stopKey;
+        private System.Windows.Forms.PictureBox muteKey;
+        private System.Windows.Forms.PictureBox lessVolumeKey;
+        private System.Windows.Forms.PictureBox moreVolumeKey;
+        private System.Windows.Forms.PictureBox removeKey;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox statusBar;
+        private System.Windows.Forms.TextBox filePath;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using DirectShowLib;
+
+namespace media_player_demo
+{
+    enum tState
+    {
+        EMPTY,
+        LOADED,
+        PAUSED,
+        STOPPED,
+        PLAYING
+    };
+    struct ttime
+    {
+        public int hour;
+        public int minute;
+        public int second;
+    };
+    public partial class Form1 : Form
+    {
+        /// Volume declarations.
+        private const int minimumVolume = -10000;
+        private const int maximumVolume = 0;
+        private bool mute = false;
+        private int volume = maximumVolume;
+        /// Graph declarations.
+        private IGraphBuilder graphBuilder = null;
+        private IMediaControl mediaControl = null;
+        private IBasicAudio basicAudio = null;
+        private IMediaSeeking mediaSeeking = null;
+        private IMediaEvent mediaEvent = null;
+        private IVideoWindow videoWindow = null;
+        /// Source type and time declarations.
+        private string fileName = string.Empty;
+        private string fileExtension = string.Empty;
+        private tState currentState = tState.EMPTY;
+        private long totalDuration = 0;
+        private long currentDuration = 0;
+        private bool isVideo = false;
+        private bool stopEvent = false;
+        private ttime playTime;
+        private ttime pauseStopTime;
+        private string storePlayTime = string.Empty;
+        private Rectangle fullScreenSize = Screen.PrimaryScreen.WorkingArea;
+        private Rectangle panelOriginalSize;
+
+        public Form1()
+        {
+            InitializeComponent();
+            /// Create graph filter manager to handle the new instance.
+            CreateAndInitializeGraph();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /// Store videoPanel's original dimensions.
+            panelOriginalSize.Size = videoPanel.Size;
+            panelOriginalSize.Location = videoPanel.Location;
+        }
+        private void openkey_Click(object sender, EventArgs e)
+        {
+            string updateStatus = string.Empty;
+            /// File formats to filter from the open-select window.
+            openFileDialog.Filter = "Common Media Files (.MP3, .MP4, .WAV, .OGG, .WMA, .AVI, .MPEG, .WMV, .MOV, .DIVX)| " +
+                                    "*.MP3;*.WAV;*.OGG;*.WMA;*.AVI;*.MP4;*.MPEG;*.WMV;*.MOV;*.DIVX;.*3GP|All Files (*.*)|*.*";
+            openFileDialog.FileName = string.Empty;
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                timer.Enabled = false;
+                fileName = openFileDialog.FileName;
+                filePath.Text = fileName;
+                pictureBox1.Visible = true;
+                /// Get file extension in UPPER case.
+                fileExtension = Path.GetExtension(fileName).ToUpper();      
+                if (fileExtension == ".WMV" || fileExtension == ".MOV" || fileExtension == ".AVI" || fileExtension == ".DIVX" ||
+                    fileExtension == ".MPEG" || fileExtension == ".MPG" || fileExtension == ".MP4" || fileExtension == ".3GP" ||
+                    fileExtension == ".3GPP" || fileExtension == ".AMV" || fileExtension == ".ASF")
+                {
+                    isVideo = true;
+                }
+                else
+                {
+                    isVideo = false;
+                }
+                /// Clear graph and all its resources.
+                Cleanup();
+                System.Threading.Thread.Sleep(50);
+                /// Create graph filter manager to handle the new instance. 
+                CreateAndInitializeGraph();
+                System.Threading.Thread.Sleep(50);
+                /// Start rendering.
+                this.graphBuilder.RenderFile(fileName, null);
+                /// Get total play time of loaded file.
+                this.mediaSeeking.GetDuration(out totalDuration);
+                TimeFormat(totalDuration, out playTime);
+                updateStatus = "File loaded.\t  Total duration = ";
+                updateStatus += playTime.hour.ToString("D" + 2) + ":";
+                updateStatus += playTime.minute.ToString("D" + 2) + ":";
+                updateStatus += playTime.second.ToString("D" + 2);
+                statusBar.Text = updateStatus;
+                storePlayTime = statusBar.Text.Substring(32, 8);
+                currentState = tState.LOADED;
+            }
+        }
+        /// Run all filters in the graph.
+        private void playKey_Click(object sender, EventArgs e)
+        {
+            string updateStatus = string.Empty;
+            if (fileName == string.Empty || currentState == tState.EMPTY)
+            {
+                return;
+            }
+            if (currentState != tState.PLAYING && currentState != tState.PAUSED)
+            {
+                /// Make videoPanel as owner of play window.
+                this.videoWindow.put_Owner(videoPanel.Handle);
+                /// Set video window style.
+                this.videoWindow.put_WindowStyle(WindowStyle.Child | WindowStyle.ClipSiblings);
+                /// Set video window dimensions.
+                this.videoWindow.SetWindowPosition(0, 0, videoPanel.Width, videoPanel.Height);
+                /// Set video window visible.
+                this.videoWindow.put_Visible(OABool.True);
+                System.Threading.Thread.Sleep(50);
+                /// video panel will handle the video window messages.
+                this.videoWindow.put_MessageDrain(videoPanel.Handle);
+            }
+            if (isVideo == true)
+            {
+                pictureBox1.Visible = false;
+            }
+            else
+            {
+                pictureBox1.Visible = true;
+            }
+            if (currentState != tState.PLAYING)
+            {
+                timer.Enabled = true;
+                System.Threading.Thread.Sleep(50);
+                /// Run all the filters in the filter graph.
+                this.mediaControl.Run();
+            }
+            /// Set volume.
+            if (mute == true)
+            {
+                updateStatus = "Playing...(Muted)      Total duration = ";
+                this.basicAudio.put_Volume(minimumVolume);
+            }
+            else
+            {
+                updateStatus = "Playing...\t\t  Total duration = ";
+                this.basicAudio.put_Volume(volume);
+            }
+            currentState = tState.PLAYING;
+            updateStatus += storePlayTime;
+            statusBar.Text = updateStatus;
+        }
+        /// Pause all filters.
+        private void pauseKey_Click(object sender, EventArgs e)
+        {
+            string updateStatus = string.Empty;
+            timer.Enabled = false;
+            if (currentState == tState.PLAYING)
+            {
+                /// Pauses all the filters in the filter graph.
+                this.mediaControl.Pause();
+                this.mediaSeeking.GetCurrentPosition(out currentDuration);
+                TimeFormat(currentDuration, out pauseStopTime);
+                updateStatus = "Paused.\t             Paused duration = ";
+                updateStatus += pauseStopTime.hour.ToString("D" + 2) + ":";
+                updateStatus += pauseStopTime.minute.ToString("D" + 2) + ":";
+                updateStatus += pauseStopTime.second.ToString("D" + 2);
+                statusBar.Text = updateStatus;
+                currentState = tState.PAUSED;
+            }
+            else
+            {
+                statusBar.Text = "First play the file.";
+            }
+        }
+        /// Stop all filters and reset graph builder.
+        private void stopKey_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            if (currentState == tState.PLAYING || currentState == tState.PAUSED)
+            {
+                /// Stops all the filters in the filter graph.
+                this.mediaControl.Stop();
+                if (stopEvent == false)
+                {
+                    statusBar.Text = "Stopped.";
+                }
+                else
+                {
+                    statusBar.Text = "Completed (Press Play to restart).";
+                    stopEvent = false;
+                }
+                pictureBox1.Visible = true;
+                pictureBox1.BringToFront();
+                currentState = tState.STOPPED;
+                /// Clear graph and all its resources.
+                Cleanup();
+                System.Threading.Thread.Sleep(50);
+                /// Create graph filter manager to handle the new instance. 
+                CreateAndInitializeGraph();
+                System.Threading.Thread.Sleep(50);
+                /// Start rendering on output pin of source filter.
+                this.graphBuilder.RenderFile(fileName, null);
+            }
+            else
+            {
+                statusBar.Text = "First play the file.";
+            }
+        }
+        /// Mute volume.
+        private void muteKey_Click(object sender, EventArgs e)
+        {
+            if (currentState != tState.EMPTY && currentState != tState.LOADED)
+            {
+                this.basicAudio.put_Volume(minimumVolume);
+            }
+            mute = true;
+            statusBar.Text = "Volume muted.";
+        }
+        /// Decrease volume.
+        private void lessVolumeKey_Click(object sender, EventArgs e)
+        {
+            mute = false;
+            volume -= 250;
+            if (volume <= minimumVolume)
+            {
+                volume = minimumVolume;
+                statusBar.Text = "Minimum Volume.";
+            }
+            else
+            {
+                statusBar.Text = "Volume decreased.";
+            }
+            if (currentState != tState.EMPTY && currentState != tState.LOADED)
+            {
+                this.basicAudio.put_Volume(volume);
+            }
+        }
+        /// Increase volume.
+        private void moreVolumeKey_Click(object sender, EventArgs e)
+        {
+            mute = false;
+            volume += 250;
+            if (volume >= maximumVolume)
+            {
+                volume = maximumVolume;
+                statusBar.Text = "Maximum Volume.";
+            }
+            else
+            {
+                statusBar.Text = "Volume increased.";
+            }
+            if (currentState != tState.EMPTY && currentState != tState.LOADED)
+            {
+                this.basicAudio.put_Volume(volume);
+            }
+        }
+        /// Restore all defaults.
+        private void removeKey_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            if (fileName != string.Empty)
+            {
+                /// Stops all the filters in the filter graph.
+                this.mediaControl.Stop();
+            }
+            pictureBox1.Visible = true;
+            pictureBox1.BringToFront();
+            fileName = string.Empty;
+            currentState = tState.EMPTY;
+            filePath.Text = "(No file selected)";
+            statusBar.Text = "All default settings restored.";
+            volume = maximumVolume;
+        }
+        /// Create graph filter manager to handle the new instance.
+        private void CreateAndInitializeGraph()
+        {
+            this.graphBuilder = (IGraphBuilder)new FilterGraph();
+            this.mediaControl = (IMediaControl)this.graphBuilder;
+            this.basicAudio = this.graphBuilder as IBasicAudio;
+            this.mediaSeeking = this.graphBuilder as IMediaSeeking;
+            this.mediaEvent = this.graphBuilder as IMediaEvent;
+            this.videoWindow = this.graphBuilder as IVideoWindow;
+        }
+        /// Convert time duration to hour, minute, second format.
+        void TimeFormat(long timeDuration, out ttime timeObject)
+        {
+            double duration;
+            /// Each unit of timeDuration is 10000000ns (MSDN).
+            duration = ((double)timeDuration) / 10000000;
+            timeObject.hour = (byte)(duration / 3600);
+            duration = duration % 3600;
+            duration = duration / 60;
+            timeObject.minute = (byte)(duration);
+            duration -= (double)timeObject.minute;
+            timeObject.second = (byte)(duration * 60);
+        }
+        /// Toggle between normal screen size and full screen size.
+        private void ToggleFullScreen(object sender, EventArgs e)
+        {
+            OABool mode;
+            if (isVideo == true && (currentState == tState.PLAYING || currentState == tState.PAUSED))
+            {
+                this.videoWindow.get_FullScreenMode(out mode);
+                if (mode == OABool.False)
+                {
+                    /// Enter full screen mode.
+                    this.videoWindow.put_FullScreenMode(OABool.True);
+                    videoPanel.Location = fullScreenSize.Location;
+                    videoPanel.Size = fullScreenSize.Size;
+                }
+                else
+                {
+                    videoPanel.Location = panelOriginalSize.Location;
+                    videoPanel.Size = panelOriginalSize.Size;
+                    /// Leave full screen mode.
+                    this.videoWindow.put_FullScreenMode(OABool.False);
+                    this.videoWindow.SetWindowPosition(0, 0, videoPanel.Width, videoPanel.Height);
+                    System.Threading.Thread.Sleep(50);
+                }
+            }
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            string updateStatus = string.Empty;
+            this.mediaSeeking.GetCurrentPosition(out currentDuration);
+            if (currentDuration >= totalDuration)
+            {
+                timer.Enabled = false;
+                stopEvent = true;
+                stopKey_Click(sender, e);
+                return;
+            }
+            TimeFormat(currentDuration, out playTime);
+            updateStatus = "Current = ";
+            updateStatus += playTime.hour.ToString("D" + 2) + ":";
+            updateStatus += playTime.minute.ToString("D" + 2) + ":";
+            updateStatus += playTime.second.ToString("D" + 2);
+            updateStatus += "\t\tTotal = " + storePlayTime;
+            statusBar.Text = updateStatus;
+        }
+        /// Clear graph and all its resources.
+        private void Cleanup()
+        {
+            Marshal.ReleaseComObject(mediaEvent);
+            Marshal.ReleaseComObject(mediaSeeking);
+            Marshal.ReleaseComObject(basicAudio);
+            Marshal.ReleaseComObject(mediaControl);
+            Marshal.ReleaseComObject(videoWindow);
+            Marshal.ReleaseComObject(graphBuilder);
+        }
+        private void Form1_closing(object sender, EventArgs e)
+        {
+            /// Clear graph and all its resources.
+            Cleanup();
+        }
+        private void closeKey_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
